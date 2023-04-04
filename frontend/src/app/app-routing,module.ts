c93713084dd9da3808b13dabfe12baab96363885
Alexander Shlyakhtin <aleksandr07011997@gmail.com>
2023-04-04 10:30:00 +0200

    feature-3. Added new page and method

--- frontend/src/app/app-routing,module.ts
+++ frontend/src/app/app-routing,module.ts
@@ -1,11 +1,15 @@
 import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
 import {UploadVideoComponent} from "./upload-video/upload-video.component";
+import {SaveVideoDetailsComponent} from "./save-video-details/save-video-details.component";
 
 
 const routes: Routes = [
   {
     path: 'upload-video', component: UploadVideoComponent
+  },
+  {
+    path: 'save-video-details/:videoId', component: SaveVideoDetailsComponent
   }
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,24 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { UploadVideoComponent } from './upload-video/upload-video.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {UploadVideoComponent} from './upload-video/upload-video.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {NgxFileDropModule} from "ngx-file-drop";
 import {RouterOutlet} from "@angular/router";
 import {AppRoutingModule} from "./app-routing,module";
-import { HeaderComponent } from './header/header.component';
+import {HeaderComponent} from './header/header.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
+import {SaveVideoDetailsComponent} from './save-video-details/save-video-details.component';
+import {MatButtonModule} from "@angular/material/button";
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     UploadVideoComponent,
-    HeaderComponent
+    HeaderComponent,
+    SaveVideoDetailsComponent
   ],
   imports: [
     BrowserModule,
@@ -28,8 +32,10 @@ import {MatIconModule} from "@angular/material/icon";
     HttpClientModule,
     NgxFileDropModule,
     RouterOutlet,
+    MatButtonModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
